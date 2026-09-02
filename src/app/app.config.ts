1705414d128a766d5fc54d64259f0402ccdd286f
maximilian27 <maximilian.both27@outlook.com>
2026-09-02 10:53:17 +0300

    chore: update deps and move to zoneless change detection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -8,6 +8,7 @@ import {provideMoltenDb} from "@moltendb-web/angular";
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
     provideRouter(routes), provideClientHydration(withEventReplay()),
     provideMoltenDb({
       name: 'docs_db',
